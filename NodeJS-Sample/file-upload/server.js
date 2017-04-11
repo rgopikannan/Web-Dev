1c48c6553dc5c0ed9b394e646405f64a9e4d3320
gopikannan ramachandran <g.ramachandran.1@elsevier.com>
2017-04-11 18:08:10 -0400

    update code

--- NodeJS-Sample/file-upload/server.js
+++ NodeJS-Sample/file-upload/server.js
@@ -5,41 +5,49 @@ var fs = require("fs");
 var bodyParser = require('body-parser');
 var multer  = require('multer');
 
-app.use(express.static('public'));
+console.log('__dirname...  '+__dirname);
+
+app.use("/", express.static(__dirname));
+//app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 //app.use(multer({ dest: '/tmp/'}));
 
 var upload = multer({ dest: 'uploads/' });
 
-var type = upload.single('recfile');
+var type = upload.single('refFile');
 
 app.get('/index.htm', function (req, res) {
-   res.sendFile( __dirname + "/" + "index.html" );
+   res.sendFile( __dirname + "/" + "upload/index.html" );
 });
 
-app.post('/file_upload', type ,function (req, res) {
-   console.log(req.file.name);
-   console.log(req.file.path);
-   console.log(req.file.type);
-   var file = __dirname + "/" + req.file.name;
+app.post('/file_upload', function (req, res) {
+   console.log(req.file);
+   //console.log(req.file.path);
+   //console.log(req.file.mimetype);
+   //var file = __dirname + "/" + req.file.fieldname;
 
+   res.end( "success...");
+   /*
    fs.readFile( req.file.path, function (err, data) {
+
+      console.log('data...   '+data);
+
       fs.writeFile(file, data, function (err) {
          if( err ){
             console.log( err );
             }else{
                response = {
                   message:'File uploaded successfully--- ',
-                  filename:req.file.name
+                  filename:req.file.fieldname
                };
             }
-         console.log( response.filename );
+         //console.log( response.filename );
          res.end( JSON.stringify( response ) );
       });
-   });
+   });*/
 });
 
-var server = app.listen(8082, function () {
+var server = app.listen(8082, '138.12.195.27', function () {
    var host = server.address().address;
    var port = server.address().port;
 
