50f7926307fa95dafdedb98e511a0f10e8452154
rgopikannan <gopikannan.psg@gmail.com>
2017-10-10 17:25:21 -0400

    Add files via upload

---
+++ IDP-Signin-popup/index.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(__dirname + '/public'));
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+
+app.post('/signin', function(req, res) {
+    var user_pwd = req.body.pwd;
+    console.log('user_pwd... '+user_pwd)
+    if(user_pwd == 'test'){      
+      res.redirect(301, '/sucess_template.html');
+    }else{
+      res.send("Invalid pwd");
+      // res.redirect(301, 'http://example.com');      
+    }   
+});
+
+
+
+app.listen(3000, function() {
+  console.log('Example app listening on port 3000!');
+});
